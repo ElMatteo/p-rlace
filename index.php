464bd29643be2c69d7012bb585d2f8265b7630e9
Matteo Clabassi <redstonemen91@gmail.com>
2022-04-10 12:12:21 -0700

    Functional ( but not final ) version of registration form and connexion form back-end

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-    include("php/db.php")
+    include("php/db.php");
+    session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -9,15 +10,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/style.css">
-    <script src="script.js"></script>
     <title>p/Rlace</title>
 </head>
 
 <body>
+    <?php
+    if(!isset($_SESSION['Username'])){
+        echo("Non connecté");
+    } else {
+        echo("Bienvenue " . $_SESSION["Username"] . " !");
+    }
 
+    if(!isset($_POST["disconnect"])) {
+        session_destroy();
+        header("index.php");
+    }
+    ?>
+    <form action="index.php" method="POST">
+    <div style='float: right;'>
+    <input type="submit" name="disconnect" value="Disconnect" id="btnDisconnect"></input>
+    </div>
+    </form>
     <div class="registrationForm">
         <h1>THE NEW p/Rlace</h1>
-        <form action="treatment.php" method="POST" class="form">
+        <form action="index.php" method="POST" class="form">
             <label for="nick">Enter your pseudo: </label>
             <input type="text" name="pseudo" id="nick" placeholder="Pseudo" required></input>
             <br><br>
@@ -30,12 +46,48 @@
             <label for="cpasswd">Confirm your password: </label>
             <input type="password" name="cpassword" id="cpasswd" placeholder="Password" required></input>
             <br><br>
-            <button type="submit">Submit
+            <a href="connexion.php">Connexion</a>
+            <br><br>
+            <input type="submit" value="Submit">
         </form>
-        <button onClick="teste()" id = "btnSubmit">Erase
+        <button onClick="erase()" id = "btnSubmit" name="sending">Erase</button>
+    <?php
+        if(isset($_POST["pseudo"]) && isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["cpassword"])){
+            if(!empty($_POST["pseudo"]) && !empty($_POST["email"]) && !empty($_POST["password"]) && !empty($_POST["cpassword"])){
+                $pseudo = $_POST['pseudo'];
+                $email = $_POST['email'];
+                $passwd = $_POST['password'];
+                $cpasswd = $_POST['cpassword'];
+                if($passwd != $cpasswd){
+                    echo("<h1 style = 'color: white;'>Password doesn't match</h1>");
+                } else {
+                    $query = "SELECT * FROM registration WHERE pseudoUser ='$pseudo' OR emailUser = '$email'";
+                    $result = mysqli_query($con,$query);
+                    if($result->num_rows > 0) {
+                        $row = mysqli_fetch_assoc($result);
+                        if($row['emailUser'] == $email){
+                            echo("<p>Error ! Email already exist !");
+                        }   
+                        echo("Email user: " . $row['emailUser']);
+                        echo("Email entered: " . $email);
+                        if ($row['pseudoUser'] == $pseudo) {
+                            echo("<p>Error ! Username already exist !");
+                        }
+                    } else {
+                        $query = "insert into registration (pseudoUser, emailUser, passwordUser) values ('$pseudo','$email','$passwd')";
+                        mysqli_query($con,$query);
+                        header("index.php");
+                    }
+                }
+            } else {
+                echo("<p style='color: white;text-align:center'>You must complete all the inputs !</p>");
+            }
+        } else {
+        }
+    ?>
     </div>
     <script>
-        function teste(){
+        function erase(){
             var test = []
             test[0] = document.getElementById("nick")
             test[1] = document.getElementById("mail")
@@ -45,6 +97,7 @@
                 test[i].value = ""
             }
         }
+
     </script>
 </body>
 
